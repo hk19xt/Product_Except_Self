c324898b513c007172100cf387b8a7c8993c12b2
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-09-02 23:40:26 -0400

    Update Main.java

--- Project_Except/src/Product_Except/Main.java
+++ Project_Except/src/Product_Except/Main.java
@@ -2,11 +2,17 @@ package Product_Except;
 
 import java.util.Arrays;
 
+/*Author: Hyejin Kim
+  Date: 9/2/2021 */
+
+//This class is main class. The users can hardcode the input numbers to calculate the project except the self element.
+
 public class Main {
 
     public static void main(String[] args){
+        
         Product_Except PE = new Product_Except();
-        int[] inputNumbers = {1,2,3,4};
+        int[] inputNumbers = {1,2,3,4};  //The input number can be changed by users.
 
         System.out.println(Arrays.toString(PE.Product_Except(inputNumbers)));
 
